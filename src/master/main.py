ffd4ce24cac6789aaeb9266c4d42f603f50233ad
Iancu Stefan-Teodor <stefaniancu46@gmail.com>
2025-12-13 16:28:53 +0200

    Master Node generating random jobs and enqueing ops

--- src/master/main.py
+++ src/master/main.py
@@ -1,15 +1,22 @@
-from master.setup import redis_connection, setup_logging
+from master.setup import redis_connection, setup_logging, QUEUE_CONFIG
 from master import helper
 import logging
+from master.helper import enqueue_random_jobs
 
 logger = setup_logging("MASTER.main")
 
 def main():
     logger.info("Starting Master Node")
-    if helper.try_ping_redis(redis_connection):
+    try:
+        helper.try_ping_redis(redis_connection)
         logger.info("Redis is reachable")
-    else:
-        logger.error("Redis is not reachable")
+
+        enqueue_random_jobs(redis_connection, QUEUE_CONFIG['stream_name'], logger)
+        
+    except Exception as e:
+        logger.exception(f"Failed to reach Redis: {e}")
+        return
+
     
 if __name__ == "__main__":
     main()
--- src/master/setup.py
+++ src/master/setup.py
@@ -6,7 +6,7 @@ import logging.config
 # Functions
 def setup_redis_connection():
     try:
-        redis_connection = redis.Redis(host='redis', port=6379, decode_responses=True)
+        redis_connection = redis.Redis(host=QUEUE_CONFIG['host'], port=QUEUE_CONFIG['port'], decode_responses=True)
         logger.info("Connected to Redis")
     except redis.exceptions.ConnectionError as e:
         logger.exception(f"Redis connection failed: {e}")
@@ -28,16 +28,40 @@ def setup_logging(name):
 
 
 # Setup phase
+
 try:
     LOGGING_CONFIG = json.load(open('src/cfg/logging.json'))
-    QUEUE_CONFIG = json.load(open('src/cfg/queue.json'))
     logger = setup_logging("MASTER.setup")
+    logger.info("Logger configuration loaded")
 
+    QUEUE_CONFIG = json.load(open('src/cfg/queue.json'))
+    logger.info("Queue configuration loaded")
 
     redis_connection = setup_redis_connection()
     groups = QUEUE_CONFIG.get("consumer_groups", [])
+    logger.info(f"Queue configuration loaded")
+
     for group in groups:
         create_consumer_group(redis_connection, group, QUEUE_CONFIG.get("stream_name"))
+        
+    logger.info("Setup completed successfully")
+
+except FileNotFoundError as e:
+    print(f"Configuration file not found: {e}")
+    raise e
+
+except json.JSONDecodeError as e:
+    print(f"Error decoding JSON configuration: {e}")
+    raise e
+
+except redis.exceptions.ConnectionError as e:
+    print(f"Redis connection error: {e}")
+    raise e
+
+except redis.exceptions.ResponseError as e:
+    print(f"Redis response error: {e}")
+    raise e
+
 except Exception as e:
     print(f"Setup failed: {e}")
     raise e
